ad4b7aad7d3828180aa9bba12520112acb2efa76
Kirti Prajapat <104522416+Kirti2704@users.noreply.github.com>
2023-05-21 01:29:41 +0530

    Add files via upload

---
+++ qone.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class qone {
+    public static void main(String[] args){
+        Frame f=new Frame();
+        Label l=new Label("Hello VIT");
+        f.add(l);
+        f.setVisible(true);
+    }
+}
