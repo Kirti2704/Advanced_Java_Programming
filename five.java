c80a64c703a6f4a1444f2e1b7f4d2107af4112a4
Kirti Prajapat <104522416+Kirti2704@users.noreply.github.com>
2023-05-21 01:27:47 +0530

    Add files via upload

---
+++ five.java
@@ -0,0 +1,7 @@
+public class five {
+    public static void main(String[] args){
+        int x1=50, x2=60, y1=30, y2=20;
+        double distance =Math.sqrt((Math.pow(x2-x1,2))+(Math.pow(y2-y1,2)));
+        System.out.println("distance is "+ distance);
+    }
+}
